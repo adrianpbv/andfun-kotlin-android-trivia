ee93961a6fa57c47e0877b650de55a8e56b638b1
adrianj <ajesusperez96@gmail.com>
2021-05-20 07:51:00 -0400

    Navigation drawer added

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,22 +19,53 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerlayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         val navController = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        drawerlayout = binding.drawerlayout
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerlayout)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerlayout)
+
+        // prevent nav gesture if not on start destination,
+        // this is the navigation listener that gets called whenever the destination changes
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, args: Bundle? ->
+            if (nd.id == nc.graph.startDestination) {
+                drawerlayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED) //you can swipe
+            } else {
+                drawerlayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+
+        // see the difference in the previous branch with git
+        // return navController.navigateUp()
+
+        return NavigationUI.navigateUp(navController,appBarConfiguration) // this is so the navigationUI can
+        // replace the Up button with the navigation drawer button when we get to the start destination
+
+        // the last return might be
+        //return NavigationUI.navigateUp(navController, drawerLayout) //works the same way
     }
 }
